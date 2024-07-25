329beac036a859f3eab1fc8d5bce9282be83d826
Brucey77 <aaaeeettt124@126.com>
2024-07-25 17:01:11 +0800

    faoiwehjfgoaipuewhgiouawehjfiojoi

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,44 +1,11 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
-import dbConnect from "@/lib/dbConnect";
-import User from "@/models/User";
-
-// 定义用户类型
-interface UserType {
-  _id: string;
-  name: string;
-  email: string;
-}
-// Fetch users function
-async function fetchUsers(): Promise<UserType[]> {
-  await dbConnect();
-  const usersData = await User.find({});
-  return usersData;
-}
+// src/app/dashboard/page.tsx
+import React from "react";
+import UsersList from "./UsersList";
 
 const Userpage = () => {
-  const [users, setUsers] = useState<UserType[]>([]);
-
-  useEffect(() => {
-    const getUsers = async () => {
-      const usersData = await fetchUsers();
-      setUsers(usersData);
-    };
-
-    getUsers();
-  }, []);
-
   return (
     <div>
-      <h1>Userpage</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user._id}>
-            {user.name} - {user.email}
-          </li>
-        ))}
-      </ul>
+      <UsersList />
     </div>
   );
 };
